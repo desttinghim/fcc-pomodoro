20bf893a5efdaaae04982ff55b0971ff7ff4327e
Louis Pearson <desttinghimgame@gmail.com>
2017-03-21 16:33:05 -0500

    Redraw when time changes

--- src/Pomodoro.hx
+++ src/Pomodoro.hx
@@ -12,23 +12,21 @@ class Pomodoro implements Mithril
     this.breakTime = breakTime;
     this.currentTime = 0;
     var secondsTimer = new haxe.Timer(1000);
-    secondsTimer.run = function() {this.currentTime += 1;}
+    secondsTimer.run = function() {this.currentTime += 1; M.redraw();}
   }
 
   public function view() [
     m('input', {
       oninput: function(e) sessionTime = e.target.value * 60,
-      value: Math.floor(sessionTime / 60)
+      value: 'Session Time: ' + Math.floor(sessionTime / 60)
     }),
     m('input', {
       oninput: function(e) breakTime = e.target.value * 60,
-      value: Math.floor(breakTime / 60)
+      value: 'Break Time' + Math.floor(breakTime / 60)
     }),
     m('.session', {}, sessionTime),
     m('.break', {}, breakTime),
-    m('.currentTime', {
-      scir
-    }, Math.floor(currentTime / 60) + ':' + currentTime % 60)
+    m('.currentTime', {}, Math.floor(currentTime / 60) + ':' + currentTime % 60)
   ];
 
   static function main() {M.mount(js.Browser.document.body, new Pomodoro(25*60, 5*60));}
