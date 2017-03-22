bc535339be473c5c93e2814b789e50384c99cf65
Louis Pearson <desttinghimgame@gmail.com>
2017-03-22 00:14:07 -0500

    Implement different working/break states

--- src/Pomodoro.hx
+++ src/Pomodoro.hx
@@ -2,6 +2,11 @@ import mithril.M;
 import haxe.Timer;
 import js.Browser;
 
+@:enum abstract PomodoroState(String) from String {
+  var Working = "Working...";
+  var Break = "Break!";
+}
+
 class Pomodoro implements Mithril {
   // time in seconds
   var startTime : Float;
@@ -10,18 +15,33 @@ class Pomodoro implements Mithril {
   var sessionTime : Spinner;
   var breakTime : Spinner;
 
+  var state : PomodoroState;
+
   public function new(sessionTime, breakTime) {
     this.sessionTime = new Spinner(sessionTime);
     this.breakTime = new Spinner(breakTime);
     this.startTime = Date.now().getTime();
     this.currentTime = startTime;
+    this.state = Working;
     var secondsTimer = new Timer(1000);
-    secondsTimer.run = function() {this.currentTime = Date.now().getTime(); M.redraw();}
+    secondsTimer.run = update;
+  }
+
+  function update() {
+    var timeDelta = currentTime - startTime;
+    state = switch (state) {
+      case Working if (timeDelta > sessionTime.value * 60000): {startTime = currentTime; Break;}
+      case Break if (timeDelta > breakTime.value * 60000): {startTime = currentTime; Working;}
+      default: state;
+    }
+    this.currentTime = Date.now().getTime();
+    M.redraw();
   }
 
   public function view() [
     m(sessionTime, {}),
     m(breakTime, {}),
+    m('p', {}, state),
     m('.currentTime', {}, DateTools.format(Date.fromTime(currentTime - startTime), "%M:%S"))
   ];
 
@@ -29,9 +49,15 @@ class Pomodoro implements Mithril {
 }
 
 class Spinner implements Mithril {
-  public var value : Int;
+
+  public var value(default, set) : Int;
+
   public function new(value=0) {
     this.value = value;
+  }
+
+  public function set_value(newValue):Int {
+    return this.value = cast Math.max(1, newValue);
   }
 
   public function view() [
